2525d979a8c3b38aeefe3a2dd4a2cc37fa280a9c
Spandan Buragohain <spandanbg@gmail.com>
2022-09-27 21:42:32 +0530

    move tests to different file

---
+++ src/analyzer/analyzer_tests.rs
@@ -0,0 +1,95 @@
+#[cfg(test)]
+mod analyzer_tests {
+    use crate::analyzer::{self, Analyzer};
+    use crate::board::{self, Coord, CellType};
+
+    #[test]
+    fn given_position_2_winning_then_return_winning_coordinate() {
+        let game_analyzer = analyzer::new();
+        let game_board = board::new();
+
+        let best_move = game_analyzer.get_best_move(
+            game_board
+                .place_player(Coord { x: 0, y: 0 })
+                .place_player(Coord { x: 1, y: 0 }),
+            CellType::PLAYER_1,
+        );
+
+        assert_eq!(best_move, Some(Coord { x: 2, y: 0 }));
+    }
+
+    #[test]
+    fn given_position_1_winning_then_return_winning_coordinate() {
+        let game_analyzer = analyzer::new();
+        let game_board = board::new();
+
+        let best_move = game_analyzer.get_best_move(
+            game_board
+                .place_player(Coord { x: 0, y: 0 })
+                .place_player(Coord { x: 2, y: 0 }),
+            CellType::PLAYER_1,
+        );
+
+        assert_eq!(best_move, Some(Coord { x: 1, y: 0 }));
+    }
+
+    #[test]
+    fn given_position_0_winning_then_return_winning_coordinate() {
+        let game_analyzer = analyzer::new();
+        let game_board = board::new();
+
+        let best_move = game_analyzer.get_best_move(
+            game_board
+                .place_player(Coord { x: 1, y: 0 })
+                .place_player(Coord { x: 2, y: 0 }),
+            CellType::PLAYER_1,
+        );
+
+        assert_eq!(best_move, Some(Coord { x: 0, y: 0 }));
+    }
+
+    #[test]
+    fn given_position_5_winning_then_return_winning_coordinate() {
+        let game_analyzer = analyzer::new();
+        let game_board = board::new();
+
+        let best_move = game_analyzer.get_best_move(
+            game_board
+                .place_player(Coord { x: 0, y: 1 })
+                .place_player(Coord { x: 1, y: 1 }),
+            CellType::PLAYER_1,
+        );
+
+        assert_eq!(best_move, Some(Coord { x: 2, y: 1 }));
+    }
+
+    #[test]
+    fn given_position_4_winning_then_return_winning_coordinate() {
+        let game_analyzer = analyzer::new();
+        let game_board = board::new();
+
+        let best_move = game_analyzer.get_best_move(
+            game_board
+                .place_player(Coord { x: 0, y: 1 })
+                .place_player(Coord { x: 2, y: 1 }),
+            CellType::PLAYER_1,
+        );
+
+        assert_eq!(best_move, Some(Coord { x: 1, y: 1 }));
+    }
+
+    #[test]
+    fn given_position_3_winning_then_return_winning_coordinate() {
+        let game_analyzer = analyzer::new();
+        let game_board = board::new();
+
+        let best_move = game_analyzer.get_best_move(
+            game_board
+                .place_player(Coord { x: 1, y: 1 })
+                .place_player(Coord { x: 2, y: 1 }),
+            CellType::PLAYER_1,
+        );
+
+        assert_eq!(best_move, Some(Coord { x: 0, y: 1 }));
+    }
+}
--- src/analyzer/mod.rs
+++ src/analyzer/mod.rs
@@ -1,3 +1,5 @@
+mod analyzer_tests;
+
 use crate::board::{self, Coord, CellType};
 
 pub struct AnalyzerState {}
@@ -59,100 +61,4 @@ fn position_to_coord(position: &usize) -> board::Coord {
     let x: isize = (*position as isize) % 3;
     let y: isize = (*position as isize) / 3;
     return board::Coord { x, y };
-}
-
-#[cfg(test)]
-mod analyzer_tests {
-    use crate::analyzer::{self, Analyzer};
-    use crate::board::{self, Coord, CellType};
-
-    #[test]
-    fn given_position_2_winning_then_return_winning_coordinate() {
-        let game_analyzer = analyzer::new();
-        let game_board = board::new();
-
-        let best_move = game_analyzer.get_best_move(
-            game_board
-                .place_player(Coord { x: 0, y: 0 })
-                .place_player(Coord { x: 1, y: 0 }),
-            CellType::PLAYER_1,
-        );
-
-        assert_eq!(best_move, Some(Coord { x: 2, y: 0 }));
-    }
-
-    #[test]
-    fn given_position_1_winning_then_return_winning_coordinate() {
-        let game_analyzer = analyzer::new();
-        let game_board = board::new();
-
-        let best_move = game_analyzer.get_best_move(
-            game_board
-                .place_player(Coord { x: 0, y: 0 })
-                .place_player(Coord { x: 2, y: 0 }),
-            CellType::PLAYER_1,
-        );
-
-        assert_eq!(best_move, Some(Coord { x: 1, y: 0 }));
-    }
-
-    #[test]
-    fn given_position_0_winning_then_return_winning_coordinate() {
-        let game_analyzer = analyzer::new();
-        let game_board = board::new();
-
-        let best_move = game_analyzer.get_best_move(
-            game_board
-                .place_player(Coord { x: 1, y: 0 })
-                .place_player(Coord { x: 2, y: 0 }),
-            CellType::PLAYER_1,
-        );
-
-        assert_eq!(best_move, Some(Coord { x: 0, y: 0 }));
-    }
-
-    #[test]
-    fn given_position_5_winning_then_return_winning_coordinate() {
-        let game_analyzer = analyzer::new();
-        let game_board = board::new();
-
-        let best_move = game_analyzer.get_best_move(
-            game_board
-                .place_player(Coord { x: 0, y: 1 })
-                .place_player(Coord { x: 1, y: 1 }),
-            CellType::PLAYER_1,
-        );
-
-        assert_eq!(best_move, Some(Coord { x: 2, y: 1 }));
-    }
-
-    #[test]
-    fn given_position_4_winning_then_return_winning_coordinate() {
-        let game_analyzer = analyzer::new();
-        let game_board = board::new();
-
-        let best_move = game_analyzer.get_best_move(
-            game_board
-                .place_player(Coord { x: 0, y: 1 })
-                .place_player(Coord { x: 2, y: 1 }),
-            CellType::PLAYER_1,
-        );
-
-        assert_eq!(best_move, Some(Coord { x: 1, y: 1 }));
-    }
-
-    #[test]
-    fn given_position_3_winning_then_return_winning_coordinate() {
-        let game_analyzer = analyzer::new();
-        let game_board = board::new();
-
-        let best_move = game_analyzer.get_best_move(
-            game_board
-                .place_player(Coord { x: 1, y: 1 })
-                .place_player(Coord { x: 2, y: 1 }),
-            CellType::PLAYER_1,
-        );
-
-        assert_eq!(best_move, Some(Coord { x: 0, y: 1 }));
-    }
 }
