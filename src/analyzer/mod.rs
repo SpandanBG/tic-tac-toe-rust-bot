887af56413c5e5d88a6ed73b2185bf48db7c6aea
Spandan Buragohain <spandanbg@gmail.com>
2022-09-27 20:03:56 +0530

    setup analyzer

--- src/analyzer/mod.rs
+++ src/analyzer/mod.rs
@@ -1,13 +1,29 @@
-use crate::board::{self, Game};
+use crate::board;
 
-pub struct Analyzer {
-  board: Box<dyn Game>
+pub struct AnalyzerState {}
+
+pub trait Analyzer {
+  fn get_best_move(game_board: Box<dyn board::Game>) -> board::Coord;
 }
 
-fn setup() -> Analyzer {
-  let board = board::new(); 
+impl Analyzer for AnalyzerState {
 
-  return Analyzer {
-    board
+  fn get_best_move(game_board: Box<dyn board::Game>) -> board::Coord {
+    return board::Coord { x: 1, y: 1 };
   }
+
+}
+
+pub fn new() -> Box<impl Analyzer> {
+  return Box::new(AnalyzerState{});
+}
+
+
+// ----------------------------- TEST ----------------------------- 
+
+#[cfg(test)]
+mod tests {
+
+  
+
 }
