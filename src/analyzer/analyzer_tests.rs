47e54af49215b3e6061d00cf5bc6bfd9ffebfc8e
Spandan Buragohain <spandanbg@gmail.com>
2022-09-30 00:05:17 +0530

    add tests for veritcal defense

--- src/analyzer/analyzer_tests.rs
+++ src/analyzer/analyzer_tests.rs
@@ -304,5 +304,77 @@ mod analyzer_tests {
             opponent_cell_type: CellType::PLAYER_2,
             expected_position_to_play: Coord { x: 2, y: 2 }
         },
+
+        given_vertically_0_defends_then_return_coordinates: TwoPositionDefendingTest {
+            position_1: Coord { x: 0, y: 1 },
+            position_2: Coord { x: 0, y: 2 },
+            player_cell_type: CellType::PLAYER_1,
+            opponent_cell_type: CellType::PLAYER_2,
+            expected_position_to_play: Coord { x: 0, y: 0 }
+        },
+
+        given_vertically_1_defends_then_return_coordinates: TwoPositionDefendingTest {
+            position_1: Coord { x: 0, y: 0 },
+            position_2: Coord { x: 0, y: 2 },
+            player_cell_type: CellType::PLAYER_1,
+            opponent_cell_type: CellType::PLAYER_2,
+            expected_position_to_play: Coord { x: 0, y: 1 }
+        },
+
+        given_vertically_2_defends_then_return_coordinates: TwoPositionDefendingTest {
+            position_1: Coord { x: 0, y: 0 },
+            position_2: Coord { x: 0, y: 1 },
+            player_cell_type: CellType::PLAYER_1,
+            opponent_cell_type: CellType::PLAYER_2,
+            expected_position_to_play: Coord { x: 0, y: 2 }
+        },
+
+        given_vertically_3_defends_then_return_coordinates: TwoPositionDefendingTest {
+            position_1: Coord { x: 1, y: 1 },
+            position_2: Coord { x: 1, y: 2 },
+            player_cell_type: CellType::PLAYER_1,
+            opponent_cell_type: CellType::PLAYER_2,
+            expected_position_to_play: Coord { x: 1, y: 0 }
+        },
+
+        given_vertically_4_defends_then_return_coordinates: TwoPositionDefendingTest {
+            position_1: Coord { x: 1, y: 0 },
+            position_2: Coord { x: 1, y: 2 },
+            player_cell_type: CellType::PLAYER_1,
+            opponent_cell_type: CellType::PLAYER_2,
+            expected_position_to_play: Coord { x: 1, y: 1 }
+        },
+
+        given_vertically_5_defends_then_return_coordinates: TwoPositionDefendingTest {
+            position_1: Coord { x: 1, y: 0 },
+            position_2: Coord { x: 1, y: 1 },
+            player_cell_type: CellType::PLAYER_1,
+            opponent_cell_type: CellType::PLAYER_2,
+            expected_position_to_play: Coord { x: 1, y: 2 }
+        },
+
+        given_vertically_6_defends_then_return_coordinates: TwoPositionDefendingTest {
+            position_1: Coord { x: 2, y: 1 },
+            position_2: Coord { x: 2, y: 2 },
+            player_cell_type: CellType::PLAYER_1,
+            opponent_cell_type: CellType::PLAYER_2,
+            expected_position_to_play: Coord { x: 2, y: 0 }
+        },
+
+        given_vertically_7_defends_then_return_coordinates: TwoPositionDefendingTest {
+            position_1: Coord { x: 2, y: 0 },
+            position_2: Coord { x: 2, y: 2 },
+            player_cell_type: CellType::PLAYER_1,
+            opponent_cell_type: CellType::PLAYER_2,
+            expected_position_to_play: Coord { x: 2, y: 1 }
+        },
+
+        given_vertically_8_defends_then_return_coordinates: TwoPositionDefendingTest {
+            position_1: Coord { x: 2, y: 0 },
+            position_2: Coord { x: 2, y: 1 },
+            player_cell_type: CellType::PLAYER_1,
+            opponent_cell_type: CellType::PLAYER_2,
+            expected_position_to_play: Coord { x: 2, y: 2 }
+        },
     }
 }
